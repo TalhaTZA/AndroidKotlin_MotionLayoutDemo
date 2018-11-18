58dd348350ca43a6c0087a0d1fb5a48fcafd1a50
TalhaTZA <talhavjtza@gmail.com>
2018-11-18 23:36:07 +0500

    ADD basic motion

--- app/src/main/java/kt/enterprises/com/mlexample/MainActivity.kt
+++ app/src/main/java/kt/enterprises/com/mlexample/MainActivity.kt
@@ -1,12 +1,39 @@
 package kt.enterprises.com.mlexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import kt.enterprises.com.mlexample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBinding = DataBindingUtil.setContentView(this@MainActivity,R.layout.activity_main)
+
+        mBinding.apply {
+            btnMotion1.setOnClickListener(this@MainActivity)
+        }
+
+
+    }
+
+    override fun onClick(view: View?) {
+
+        when (view?.id){
+            null ->{}
+
+            R.id.btn_motion_1 ->{
+                startActivity(Intent(this@MainActivity,MotionBasic1Activity::class.java))
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/kt/enterprises/com/mlexample/MotionBasic1Activity.kt
@@ -0,0 +1,14 @@
+package kt.enterprises.com.mlexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionBasic1Activity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_basic1)
+
+    }
+}
